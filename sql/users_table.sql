ef0eaadbc59517404142b83c59cb0e3d85bae3bb
Robztar <71354535+Robztar@users.noreply.github.com>
2021-10-28 11:09:53 -0500

    Update users_table.sql

--- sql/users_table.sql
+++ sql/users_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE users ( 
 	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	fname varchar(100) NOT NULL, 
-    lname varchar(100) NOT NULL, 
+	lname varchar(100) NOT NULL, 
 	email varchar(100) NOT NULL, 
-	pass varchar(100) NOT NULL,
-
-) 
+	pass varchar(100) NOT NULL
+);
