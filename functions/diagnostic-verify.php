a2883aded2d2a04bd7c6feacd915eace9c200329
Robztar <71354535+Robztar@users.noreply.github.com>
2021-11-09 19:07:37 -0500

    Diagnostics Test Pt.1 - (Trial 1)
    Set up the page and questions for the diagnostics test.
    Set basic design (not refined or final).
    Set up data collection.
    No verification done as yet.

---
+++ functions/diagnostic-verify.php
@@ -0,0 +1,53 @@
+<?php
+     // 1. Fill in the blanks: A __ C
+     // 2. Fill in the blanks: ___ M N
+     // 3. Circle the vowel: S I P
+     // 4. Circle the vowel: X Y Z
+     // 5. Which vowels are in this word? Giraffe
+     // 6. Choose a letter to complete the word: Hi__e (o, y, d)
+     // 7. Choose a letter to complete the word: __hip (T, R, C)
+     // 8. What part of speech is the highlighted word in the sentence: They visit the 
+     // countryside often. (Pronoun, Noun, Adverb)
+     // 9. She sleeps all the time. (Noun, Pronoun, Adjective)
+     // 10. Which part of this sentence is highlighted? They ran away (Subject/ Predicate)
+     // 11. Which part of this sentence is highlighted? Dinah and her cat left the house. (Subject/ 
+     // Predicate)
+     // 12. Is this a complete sentence? Running here and there (Yes/ No)
+     // 13. Is this a complete sentence? They played. (Yes/ No)
+     // 14. What figure of speech is shown by this sentence? The flowers danced in the wind. 
+     // (simile, metaphor, personification)
+     // 15. The child turned as pale as a white sheet. (simile, metaphor, personification)
+     // 16. Select the correct word to complete the sentence: They (has, have) beautiful dolls.
+     // 17. Select the correct word to complete the sentence: None of the boys (know, knows) 
+     // where the money is.
+     // 18. Add the correct punctuation to the end of this sentence: Is Rohan your friend (./!/?)
+     // 19. Wow (./!/?)
+     // 20. What is the full form of the abbreviation Mon.? (Monkey, Monday, Money
+
+
+     // Answers
+     // 1. b
+     // 2. l
+     // 3. i
+     // 4. y
+     // 5. i,a,e (array)
+     // 6. d
+     // 7. c
+     // 8. adverb
+     // 9. pronoun
+     // 10. subject
+     // 11. predicate
+     // 12. no
+     // 13. yes
+     // 14. metaphor
+     // 15. simile
+     // 16. have
+     // 17. know
+     // 18. ?
+     // 19. !
+     // 20. monday
+
+     // diagnostics database fields:
+     // questionid (same as question number)
+     // correctanswer (answer(s) for the question)
+     // ?givenanswer (answer(s) given by the user)
---
+++ js/main.js
@@ -0,0 +1,78 @@
+let ansArr = [];
+var choice;
+var allChoice = [];
+
+function nextQ(x){
+     $("section").css('display','none');
+     $(x).css('display','flex');
+}
+
+function saveAns(x){
+     ansArr.push(x);
+}
+
+function submit(x,y){
+     nextQ(x);
+     saveAns(y);
+     console.log(ansArr);
+}
+
+$('.choice').click(function(e){
+     choice = e.target.value;
+     console.log(choice);
+});
+
+$('.selects').click(function(e){
+     // create a deselect option
+     allChoice.push(e.target.value) ;
+     console.log(allChoice);
+});
+
+
+
+
+
+console.log(ansArr);
+
+
+
+// $(".lname-next").click(function(){
+//      // Check if input is valid
+
+//      $( "label[name = 'lname']" ).css('display','block');
+//      $( "input[name = 'lname']" ).css('display','inline');
+//      $( ".email-next" ).css('display','inline');
+// });
+
+// $(".email-next").click(function(){
+//      // Check if input is valid
+
+//      $( "label[name = 'email']" ).css('display','block');
+//      $( "input[name = 'email']" ).css('display','inline');
+//      $( ".pwd-next" ).css('display','inline');
+// });
+
+// $(".pwd-next").click(function(){
+//      // Check if input is valid
+
+//      $( "label[name = 'pwd']" ).css('display','block');
+//      $( "input[name = 'pwd']" ).css('display','inline');
+//      $( ".cpwd-next" ).css('display','inline');
+// });
+
+// $(".cpwd-next").click(function(){
+//      // Check if input is valid
+
+//      $( "label[name *= 'cpwd']" ).css('display','block');
+//      $( "input[name *= 'cpwd']" ).css('display','inline');
+
+//      $( ".signup-next" ).css('display','inline');
+//      $( ".login-next" ).css('display','inline');
+// });
+
+// $( ".signup-next" ).click(function(){
+//      $( ".sub-btn" ).css('display','flex');
+// });
+// $( ".login-next" ).click(function(){
+//      $( ".sub-btn" ).css('display','flex');
+// });
