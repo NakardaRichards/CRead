66dec828c579e3c08793cbbe0df4a370ea9bc87b
Robztar <71354535+Robztar@users.noreply.github.com>
2021-11-30 09:50:52 -0500

    Lessons Edit
    Remove link to other lessons
    Add link to each lesson's test

--- dashboard/advancedlessons3.php
+++ dashboard/advancedlessons3.php
@@ -14,6 +14,23 @@
 
 
     <title>Advanced Lessons</title>
+    <style>
+      .go-to-test{
+          margin: 2rem;
+          text-decoration: none;
+          border-radius: 10px;
+          color: white;
+          padding: 0.5rem;
+          background: #ff00c8;
+          transition: 0.5s ease;
+      }
+      .go-to-test:hover{
+          text-decoration: none;
+          color: inherit;
+          background: cyan;
+          transition: 0.5s ease;
+      }
+    </style>
 </head>
 <body>
 <div id="navigationbar">
@@ -28,22 +45,25 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                
-                
-                <a class="nav-link" href="../dashboard/advancedlessons.php">Lesson 1</a>
-                <a class="nav-link" href="../dashboard/advancedlessons2.php">Lesson 2</a>
-                <a class="nav-link" href="../dashboard/advancedlessons3.php">Lesson 3</a>
-               
-                
-               
-              </div>
+              <a class="nav-link" href="../dashboard/Dashboard.php">Home</a>
+              <a class="nav-link" href="../pages/about.html">About Us</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page" href="#">Supplementary Material</a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="../Supplemental Material Page/smaterial.html#beginner">Beginner Resources</a></li>
+                  <li><a class="dropdown-item" href="../Supplemental Material Page/smaterial.html#intermediate">Intermediate Resources</a></li>
+                  <li><a class="dropdown-item" href="../Supplemental Material Page/smaterial.html#advanced">Advanced Resources</a></li>
+                </ul>
+              </li>
+              <a class="nav-link" href="../pages/contact.html">Contact Us</a>
             </div>
+          </div>
           </div>
         </nav>
       </div>
       <br>
       <div id="lesson_1" style=" text-align:center; ">
-    <h1 style="text-align: center;">Lesson 3: Contractions and Abbreviations</h1>
+    <h1 style="text-align: center;">Advanced Lesson 3: Contractions and Abbreviations</h1>
     
 <iframe  width="400px" height="300px" style="border:2px solid red; margin:15px; margin-top:50px" src="https://www.youtube.com/embed/vnB4Eu7X1Qg" title="Lesson 2: Punctuations" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe  width="400px" height="300px" style="border:2px solid red; margin:15px; margin-top:50px" src="https://www.youtube.com/embed/M-6OVp5p9Y8" title="Lesson 2: Punctuations" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -51,13 +71,13 @@
         <iframe  width="400px" height="300px" style="border:2px solid red; margin:15px; margin-top:80px" src="https://www.youtube.com/embed/q6J6wf_iEpw" title="Lesson 2: Punctuations" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
        
 
-    
+          <br><a class="go-to-test" href="../tests/advanced-t3.php">Go To Test</a>
 </div>
 
 
 
 
-
-</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
